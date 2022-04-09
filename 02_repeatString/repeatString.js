adbbdc0af1c0ab89ee0819f0ad517e5089654fc9
HasnatSN <hasnat.kontakt@gmail.com>
2022-04-09 22:11:38 +0200

    die ersten 2 Beispiele gemacht

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (text, times) {
+  if (times >= 0) {
+    let output = "";
+    for (let i = 0; i < times; i++) {
+      output += text;
+    }
 
+    return output;
+  } else {
+      let output = "ERROR";
+      return output;
+  }
 };
 
 // Do not edit below this line
