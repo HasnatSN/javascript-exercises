17146831471e2453bdeae54a099eb5e55a0b50be
HasnatSN <hasnat.kontakt@gmail.com>
2022-04-10 17:51:30 +0200

    finished exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,19 @@
-const leapYears = function() {
+const leapYears = function (u_year) {
+  let is_leap = false;
 
+  if (u_year % 4 == 0) {
+    is_leap = true;
+
+    if (u_year % 100 == 0) {
+      is_leap = false;
+
+      if (u_year % 400 == 0) {
+        is_leap = true;
+      }
+    }
+  }
+
+  return is_leap;
 };
 
 // Do not edit below this line
