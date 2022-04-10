de0fbc02eec591519a46c77053e4411ee2de8af9
HasnatSN <hasnat.kontakt@gmail.com>
2022-04-10 17:09:43 +0200

    finished exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(u_array, ...u_arg) {
+
+
+    //////////////// Remove one single instance of the argument
+    // index_of_arg = u_array.indexOf(u_arg);
+    // u_array.splice(index_of_arg, 1);
+    // return u_array;
+
+    for (arg of u_arg) {
+        for (item of u_array) {
+            if (item === arg) {
+                index_of_arg = u_array.indexOf(item);
+                u_array.splice(index_of_arg, 1);
+            };
+        };
+    }
+
+    return u_array;
 
 };
 
