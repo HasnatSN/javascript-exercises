a75f196c678e490f14c129cb9c4cb1252ad89e9f
HasnatSN <hasnat.kontakt@gmail.com>
2022-04-10 17:29:07 +0200

    finished exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function (n1, n2) {
+  if (n1 < 0 || n2 < 0) {
+    return (result = "ERROR");
+  } else if (!Number.isInteger(n1) || !Number.isInteger(n2)) {
+    return (result = "ERROR");
+  } else {
+    smaller_n = Math.min(n1, n2);
+    bigger_n = Math.max(n1, n2);
 
+    let result = 0;
+
+    for (i = smaller_n; i <= bigger_n; i++) {
+      result += i;
+    }
+
+    return result;
+  }
 };
 
 // Do not edit below this line
