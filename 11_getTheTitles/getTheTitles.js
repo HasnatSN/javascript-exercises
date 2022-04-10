9de96e7bac55d62fba75b39f01f8ee51d7bbd0d0
HasnatSN <hasnat.kontakt@gmail.com>
2022-04-10 21:24:41 +0200

    finished exercise 11, easy-easy

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function (array_of_objects) {
+  titles = [];
 
+  for (object of array_of_objects) {
+    titles.push(object.title);
+  }
+
+  return titles;
 };
 
 // Do not edit below this line
