b3bb815ff2234b684814b5adf7376db6f0b52ffc
HasnatSN <hasnat.kontakt@gmail.com>
2022-04-10 21:02:52 +0200

    finished exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function (u_position) {
+  fibonacci_array = [];
 
+  if (u_position > 0) {
+    for (i = 0; i < u_position + 1; i++) {
+      if (i < 2) {
+        fibonacci_array.push(1);
+      } else {
+        fibonacci_array.push(
+          fibonacci_array[fibonacci_array.length - 1] +
+            fibonacci_array[fibonacci_array.length - 2]
+        );
+      }
+    }
+
+    return fibonacci_array[u_position - 1];
+  } else {
+    return "OOPS";
+  }
 };
 
 // Do not edit below this line
