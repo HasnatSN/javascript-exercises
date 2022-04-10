7245054ed55c662ed592d90e0f68d268abcc39e2
HasnatSN <hasnat.kontakt@gmail.com>
2022-04-10 16:12:30 +0200

    finished exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,15 @@
-const reverseString = function() {
+const reverseString = function(u_string) {
+
+    let a_string = u_string.split('');
+    let reversed_string = [];
+    let loop_range = a_string.length;
+
+    for (i = 0; i < loop_range; i++) {
+        reversed_string.push(a_string[a_string.length - 1]);
+        a_string.pop();
+    };
+    let output = reversed_string.join('');
+    return output;
 
 };
 
