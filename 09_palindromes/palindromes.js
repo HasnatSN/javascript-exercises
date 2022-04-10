4d61c137a35e84f6fc716249a3413a765a08f8d7
HasnatSN <hasnat.kontakt@gmail.com>
2022-04-10 20:30:19 +0200

    finished exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
+const palindromes = function (u_string) {
+  u_string = u_string.replace(/\W/g, "");
+  u_string = u_string.toLowerCase();
+  u_string = u_string.replace(/\s/g, "");
+  u_array = u_string.split("");
 
+  let i = 1;
+  let reversed_array = [];
+
+  for (letter of u_array) {
+    reversed_array.push(u_array[u_array.length - i]);
+    i++;
+  }
+
+  let reversed_string = reversed_array.join("");
+
+  if (u_string == reversed_string) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
