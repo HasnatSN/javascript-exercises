9aa3b7a81eb5408f2bb5fd1aaed7a9c3619be046
HasnatSN <hasnat.kontakt@gmail.com>
2022-04-10 22:21:19 +0200

    finished exercise 12, easy-easy but took me time to understand  what the specification wanted returned

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function (array_of_objects) {
+  let age = 0;
+  let o_name = "";
 
+  for (object of array_of_objects) {
+    if (object.hasOwnProperty("yearOfDeath")) {
+      if (object.yearOfDeath - object.yearOfBirth > age) {
+        age = object.yearOfDeath - object.yearOfBirth;
+        correct_person = object;
+      }
+    } else {
+      if (2022 - object.yearOfBirth > age) {
+        age = 2022 - object.yearOfBirth;
+        correct_person = object;
+      }
+    }
+  }
+
+  return correct_person;
 };
 
 // Do not edit below this line
